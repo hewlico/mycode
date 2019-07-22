c404579ac4918fde97bcab7c1ad2cca433cbafe4
hewlico <corey.hewling@verizon.com>
2019-07-22 14:34:15 -0400

    update

---
+++ iptaker01.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+user_input = input("Please enter IPv4:")
+print("This is entered IPv4 address:" + user_input)
+
---
+++ iptaker02.py
@@ -0,0 +1,5 @@
+#1/usr/bin/env python3 
+user_vendor = input("Enter vendor info:")
+user_ip = input("Enter IPv4 address:")
+print("Entered vendor name:" + user_vendor)
+print("Entered IPv4 address:" + user_ip)
