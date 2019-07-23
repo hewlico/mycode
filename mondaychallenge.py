467e9a0d5898841128dd1aa9e38944f25ccf6727
hewlico <corey.hewling@verizon.com>
2019-07-23 08:31:36 -0400

    Python HW for Monday

--- mondaychallenge.py
+++ mondaychallenge.py
@@ -1,3 +1,4 @@
 #!/usr/bin/env python3
 pets = [2,"cats",3,"dogs"]
-print("I have", str(pets[0]),str(pets[1]) , " and ", str(pets[2]),str(pets[3]+"."))
+#print("I have", str(pets[0]),str(pets[1]) , " and ", str(pets[2]),str(pets[3]+"."))
+print("I have {2} {1} and {0} {3}.".format(*pets))
